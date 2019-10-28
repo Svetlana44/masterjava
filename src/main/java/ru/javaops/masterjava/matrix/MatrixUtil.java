ff77bc0fabe483e4c9b675d8850a5fafbdfd2d98
alabra <alllexe@mail.ru>
2019-10-28 13:46:49 +0300

    hw 01

--- src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
+++ src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
@@ -1,7 +1,10 @@
 package ru.javaops.masterjava.matrix;
 
 import java.util.Random;
+import java.util.concurrent.CompletionService;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorCompletionService;
 import java.util.concurrent.ExecutorService;
 
 /**
@@ -10,12 +13,78 @@ import java.util.concurrent.ExecutorService;
  */
 public class MatrixUtil {
 
+    static class SubResult implements Runnable {
+
+        final int[][] matrixA;
+        final int[][] matrixB;
+        final int[][] matrixC;
+        int from;
+        int to;
+        CountDownLatch latch;
+        final int matrixSize;
+
+        public SubResult(int[][] matrixA, int[][] matrixB, int[][] matrixC, int from, int to,
+            CountDownLatch latch) {
+            this.matrixA = matrixA;
+            this.matrixB = matrixB;
+            this.matrixC = matrixC;
+            this.from = from;
+            this.to = to;
+            this.latch = latch;
+            matrixSize = matrixA.length;
+        }
+
+        @Override
+        public void run() {
+            for (int i = from; i < to; i++) {
+
+                for (int j = 0; j < matrixSize; j++) {
+                    int sum = 0;
+                    for (int k = 0; k < matrixSize; k++) {
+                        sum += matrixA[i][k] * matrixB[k][j];
+                    }
+                    matrixC[i][j] = sum;
+                }
+            }
+            latch.countDown();
+        }
+    }
+
     // TODO implement parallel multiplication matrixA*matrixB
     public static int[][] concurrentMultiply(int[][] matrixA, int[][] matrixB, ExecutorService executor) throws InterruptedException, ExecutionException {
+
+        final CompletionService<Boolean> completionService = new ExecutorCompletionService<>(executor);
+
         final int matrixSize = matrixA.length;
         final int[][] matrixC = new int[matrixSize][matrixSize];
 
+        CountDownLatch latch = new CountDownLatch(10);
+
+        for (int i = 0; i < 10; i++) {
+            executor.execute(new SubResult(matrixA, matrixB, matrixC, i * 100, (i + 1) * 100, latch));
+        }
+
+        try {
+            latch.await();
+        } catch (InterruptedException E) {
+            // handle
+        }
+
         return matrixC;
+    }
+
+    public Boolean MultSubResult(int[][] matrixC, int[][] matrixA, int[][] matrixB, int i, int from, int to) {
+        Boolean result = false;
+        final int matrixSize = matrixA.length;
+        for (int j = from; j < to; j++) {
+            int sum = 0;
+            for (int k = 0; k < matrixSize; k++) {
+                sum += matrixA[i][k] * matrixB[k][j];
+            }
+            matrixC[i][j] = sum;
+        }
+        result = true;
+        return result;
     }
 
     // TODO optimize by https://habrahabr.ru/post/114797/
